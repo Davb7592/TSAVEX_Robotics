b73351b108237a2ccbfb296b93ea4a3f48ef7d02
IceLuigi7592 <82611154+IceLuigi7592@users.noreply.github.com>
2025-09-13 19:25:27 -0500

    create new proj

---
+++ v1Proj/src/main.py
@@ -0,0 +1,31 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       david                                                        #
+# 	Created:      9/13/2025, 7:23:17 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+brain = Brain()
+
+def autonomous():
+    brain.screen.clear_screen()
+    brain.screen.print("autonomous code")
+    # place automonous code here
+
+def user_control():
+    brain.screen.clear_screen()
+    brain.screen.print("driver control")
+    # place driver control in this while loop
+    while True:
+        wait(20, MSEC)
+
+# create competition instance
+comp = Competition(user_control, autonomous)
+
+# actions to do when the program starts
+brain.screen.clear_screen()
