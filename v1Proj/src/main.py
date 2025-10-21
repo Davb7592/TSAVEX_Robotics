abeeb89198c8f9d40f937124011f0c8575f12dd4
DAVID J. BARDAN <db190218@students.lcisd.org>
2025-10-20 19:47:33 -0500

    add basic movement !!!

--- v1Proj/src/main.py
+++ v1Proj/src/main.py
@@ -22,6 +22,41 @@ def user_control():
     brain.screen.print("driver control")
     # place driver control in this while loop
     while True:
+        rightX = controller.axis1.position()
+        rightY = controller.axis2.position()
+        leftX = controller.axis4.position()
+        leftY = controller.axis3.position()
+        print("\n\n\n\n\n\n\n\n\n\n")
+        print(rightX)
+        print(rightY)
+        print(leftX)
+        print(leftY)
+
+        finalLeftMotorPow, finalRightMotorPow = leftY, leftY
+        
+        
+        if leftY > 0:
+            if rightX > 0:
+                finalRightMotorPow -= 0.75 * rightX
+            elif rightX < 0:
+                finalLeftMotorPow += 0.75 * rightX
+            leftMotor.spin(FORWARD, finalLeftMotorPow)
+            rightMotor.spin(REVERSE, finalRightMotorPow)
+        elif leftY < 0:
+            if rightX > 0:
+                finalRightMotorPow += 0.75 * rightX
+            elif rightX < 0:
+                finalLeftMotorPow -= 0.75 * rightX
+            leftMotor.spin(FORWARD, finalLeftMotorPow)
+            rightMotor.spin(REVERSE, finalRightMotorPow)
+        else:
+            leftMotor.stop()
+            rightMotor.stop()
+        
+
+
+        
+
         wait(20, MSEC)
 
 # create competition instance
@@ -29,7 +64,10 @@ comp = Competition(user_control, autonomous)
 
 # actions to do when the program starts
 brain.screen.clear_screen()
-motor14 = Motor(Ports.PORT14)
-motor14.spin(FORWARD)
-wait(2, SECONDS)
-motor14.stop()
+controller = Controller()
+
+leftMotor = Motor(1)
+rightMotor = Motor(2)
+finalLeftMotorPow = 0
+finalRightMotorPow = 0
+print("a")
